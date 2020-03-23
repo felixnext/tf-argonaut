5626645482b598f08d831d68a8ba326d133eb4dc
Felix Geilert <root92@live.de>
2020-03-23 06:49:06 +0100

    Feat: updated init scripts / added setup.py

--- argonaut/experiments/__init__.py
+++ argonaut/experiments/__init__.py
@@ -1,4 +1,2 @@
 from .baseline import *
-from .elastic_weights import *
-from .dynamic_weights import *
-from .hard_attention import *
+from .experiment import *
--- argonaut/models/__init__.py
+++ argonaut/models/__init__.py
@@ -1,6 +1,5 @@
 from . import simple
 from . import densenet
-from . import dwa_nets
 from . import resnetv2
 from . import vgg16
 from . import alexnet
